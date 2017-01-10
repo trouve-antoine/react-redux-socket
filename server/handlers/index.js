ae95f5b793edc153215ee105ec56d8df3c998e73
Antoine TROUVE <trouve@roz-aibod.local>
2017-01-10 12:48:20 +0900

    Move source files

---
+++ server/handlers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  authenticate: require('./authenticate'),
+  joinRoom: require('./join-room'),
+  logConnection: require('./log-connection')
+}
---
+++ server/handlers/log-connection.js
@@ -0,0 +1,19 @@
+module.exports = _log => {
+  let log = _log
+  const handler = function(action, { emit, broadcast, socket, io }) {
+    switch(action.type) {
+      case 'SOCKET_CONNECTED': {
+        log && console.log('connected:', socket.id, "in room", action.socket_meta.room_name);
+        break
+      }
+      case 'SOCKET_DISCONNECTED': {
+        log && console.log('a user disconnected');
+        break
+      }
+    }
+  }
+
+  handler.log = _log => { log = _log; return handler }
+
+  return handler
+}
